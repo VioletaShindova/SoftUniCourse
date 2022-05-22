e8915e19d5c09d697b63aa9b0b77ef36d87d7fba
VioletaShindova <violetashindovaa@gmail.com>
2022-05-22 19:13:51 +0300

    Project for biggest number.
    The project was given for homework.

---
+++ BobiZadSArrayAndClass/BiggestNumberInTheArrayAndClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggestNumberInTheArrayAndClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input length of the array: ");
+            int lengthOfTheArray = int.Parse(Console.ReadLine());
+
+            Array array = new Array(lengthOfTheArray);
+
+            int biggestNumber = array.FindTheBiggestNumber();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"The biggest number is {biggestNumber}");
+        }
+    }
+}
