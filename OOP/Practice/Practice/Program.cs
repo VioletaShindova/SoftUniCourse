0f4ba641d0bc7de41afc11234af63556e3408fd7
VioletaShindova <violetashindovaa@gmail.com>
2022-05-23 01:15:15 +0300

    More exercises.

---
+++ OOP/Practice/Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student stud = new Student();
+
+            Console.Write("Input math grade: ");
+            stud.Math = double.Parse(Console.ReadLine());
+
+            Console.Write("Input english grade: ");
+            stud.English = double.Parse(Console.ReadLine());
+
+            Console.Write("Input science grade: ");
+            stud.Science = double.Parse(Console.ReadLine());
+
+            double averageGrade = stud.CalcAverage(stud.Math, stud.English, stud.Science);
+
+            stud.Spravka(stud.Math, stud.English, stud.Science, averageGrade);
+        }
+    }
+}
---
+++ OOP/Practice/Practice/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Student
+    {
+        double math;
+        double english;
+        double science;
+
+        public double CalcAverage(double subject1, double subject2, double subject3)
+        {
+            return (subject1 + subject2 + subject3) / 3;
+        }
+        public void Spravka (double math, double english, double science, double averageGrade)
+        {
+            Console.WriteLine("Math grade: " + math);
+            Console.WriteLine("English grade: " + english);
+            Console.WriteLine("Science grade: " + science);
+            Console.Write("Average grade: " + averageGrade);
+        }
+
+        public double Math
+        {
+            set
+            {
+                if (value > 1 && value < 7)
+                    this.math = value;
+            }
+            get
+            {
+                return this.math;
+            }
+        }
+        public double English
+        {
+            set
+            {
+                if (value > 1 && value < 7)
+                    this.english = value;
+            }
+            get
+            {
+                return this.english;
+            }
+        }
+        public double Science
+        {
+            set
+            {
+                if (value >1 && value <7)
+                this.science = value;
+            }
+            get
+            {
+                return this.science;
+            }
+        }
+    }
+}
