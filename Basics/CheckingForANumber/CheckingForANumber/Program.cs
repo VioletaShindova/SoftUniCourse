5fba6ab36e44e4e445bb829fded12b397a07da06
VioletaShindova <violetashindovaa@gmail.com>
2022-05-29 23:07:45 +0300

    Checks for a number.

---
+++ Basics/CheckingForANumber/CheckingForANumber/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckingForANumber
+{
+    internal class Program
+    {
+        static bool CheckNumber(int a, int b, int number)
+        {
+            
+            if(number>=a && number<=b || number>=b && number<=a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+
+            string text;
+            int sum;
+            int number;
+            int br = 0;
+
+            do
+            {
+                text = Console.ReadLine();
+                if (text == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Input number = ");
+                    number = int.Parse(text);
+                }
+
+                if (CheckNumber(a, b, number))
+                {
+                    sum += number;
+                    br++;
+                }
+            }
+            while (text != "stop");
+        }
+    }
+}
