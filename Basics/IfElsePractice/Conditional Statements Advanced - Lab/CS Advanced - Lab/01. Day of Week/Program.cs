2a3a20aa9944939294920b2e2f8fdde17bb85e41
VioletaShindova <violetashindovaa@gmail.com>
2022-06-03 23:39:59 +0300

    Not finished.

--- Basics/IfElsePractice/Conditional Statements Advanced - Lab/CS Advanced - Lab/01. Day of Week/Program.cs
+++ Basics/IfElsePractice/Conditional Statements Advanced - Lab/CS Advanced - Lab/01. Day of Week/Program.cs
@@ -12,23 +12,33 @@ namespace _01.Day_of_Week
         {
             int n = int.Parse(Console.ReadLine());
 
-            if (n > 0 && n < 8)
-            {
                 switch(n)
                 {
-                    case 1: Console.WriteLine("Monday"); break;
-                    case 2: Console.WriteLine("Tuesday"); break;
-                    case 3: Console.WriteLine("Wednesday"); break;
-                    case 4: Console.WriteLine("Thursday"); break;
-                    case 5: Console.WriteLine("Friday"); break;
-                    case 6: Console.WriteLine("Saturday"); break;
-                    case 7: Console.WriteLine("Sunday"); break;
+                case 1: Console.WriteLine("Monday"); 
+                break;
+
+                case 2: Console.WriteLine("Tuesday"); 
+                break;
+
+                case 3: Console.WriteLine("Wednesday"); 
+                break;
+
+                case 4: Console.WriteLine("Thursday"); 
+                break;
+
+                case 5: Console.WriteLine("Friday"); 
+                break;
+
+                case 6: Console.WriteLine("Saturday"); 
+                break;
+
+                case 7: Console.WriteLine("Sunday");
+                break;
+
+                default:
+                    Console.Write("Error");
+                    break;
                 }
-            }
-            else
-            {
-                Console.WriteLine("Error");
-            }
         }
     }
 }
