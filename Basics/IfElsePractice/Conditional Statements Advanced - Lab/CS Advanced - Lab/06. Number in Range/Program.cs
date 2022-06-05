976a3dddd07732b8420d4172270a54d66b982ef6
VioletaShindova <violetashindovaa@gmail.com>
2022-06-06 00:46:17 +0300

    6th exercise.

---
+++ Basics/IfElsePractice/Conditional Statements Advanced - Lab/CS Advanced - Lab/06. Number in Range/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Number_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number >= -100 && number <= 100 && number != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
