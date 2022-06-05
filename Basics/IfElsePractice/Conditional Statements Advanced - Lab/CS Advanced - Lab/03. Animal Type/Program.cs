ca9d67d7211179b3f8c22d0420cd2385b5250ac8
VioletaShindova <violetashindovaa@gmail.com>
2022-06-06 00:15:28 +0300

    3rd exercise.

---
+++ Basics/IfElsePractice/Conditional Statements Advanced - Lab/CS Advanced - Lab/03. Animal Type/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Animal_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            switch(animal)
+            {
+                case "dog":
+                    Console.Write("mammal");
+                    break;
+                case "snake":
+                case "crocodile":
+                case "tortoise":
+                    Console.Write("reptile");
+                    break;
+                default:
+                    Console.Write("unknown");
+                    break;
+            }
+        }
+    }
+}
--- Basics/IfElsePractice/Conditional Statements Advanced - Lab/CS Advanced - Lab/2WeekendOrWorkingDay/Program.cs
+++ Basics/IfElsePractice/Conditional Statements Advanced - Lab/CS Advanced - Lab/2WeekendOrWorkingDay/Program.cs
@@ -10,8 +10,40 @@ namespace _2WeekendOrWorkingDay
     {
         static void Main(string[] args)
         {
-                
+            string textDay = Console.ReadLine();
 
+            if(textDay == "Monday")
+            {
+                Console.Write("Working day");
+            }
+            else if(textDay == "Tuesday")
+            {
+                Console.Write("Working day");
+            }
+            else if (textDay == "Wednesday")
+            {
+                Console.Write("Working day");
+            }
+            else if (textDay == "Thursday")
+            {
+                Console.Write("Working day");
+            }
+            else if (textDay == "Friday")
+            {
+                Console.Write("Working day");
+            }
+            else if(textDay == "Saturday")
+            {
+                Console.Write("Weekend");
+            }
+            else if (textDay == "Sunday")
+            {
+                Console.Write("Weekend");
+            }
+            else
+            {
+                Console.Write("Error");
+            }
         }
     }
 }
