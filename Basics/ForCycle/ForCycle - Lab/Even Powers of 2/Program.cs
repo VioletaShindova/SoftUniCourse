fb15e4c569d1218579af049e7d3317fb5d096635
VioletaShindova <violetashindovaa@gmail.com>
2022-06-09 11:40:12 +0300

    For Loop - Lab.

---
+++ Basics/ForCycle/ForCycle - Lab/Even Powers of 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Powers_of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if(n % 2 == 0)
+            {
+                for (int i = 0; i <= n; i += 2)
+                {
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+
+            else
+            {
+                for (int i = 0; i < n; i += 2)
+                {
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+        }
+    }
+}
---
+++ Basics/ForCycle/ForCycle - Lab/Sum Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numbers;
+            int sum = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                numbers = int.Parse(Console.ReadLine());
+                sum = sum + numbers;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
