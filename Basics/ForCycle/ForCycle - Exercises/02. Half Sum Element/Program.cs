148535ec64ef8b27e5e434c65b9d80e4fbec5bcb
VioletaShindova <violetashindovaa@gmail.com>
2022-06-13 00:30:26 +0300

    hald second.

---
+++ Basics/ForCycle/ForCycle - Exercises/02. Half Sum Element/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int maxValue = int.MinValue;
+
+            for (int i = 1; i<=numberOfNumbers; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum = sum + num;
+
+                if (num>maxValue)
+                {
+                    maxValue = num;
+                }
+            }
+            int sumWithoutMaxNumber = sum - maxValue;
+            if (maxValue == sumWithoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + maxValue);
+            }
+            else
+            {
+                int diff = 
+            }
+        }
+    }
+}
