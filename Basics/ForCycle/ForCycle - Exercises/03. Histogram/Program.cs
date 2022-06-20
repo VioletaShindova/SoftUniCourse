b3d9888575b829d24a2bd02fa737827903d5aef4
VioletaShindova <violetashindovaa@gmail.com>
2022-06-20 13:12:02 +0300

    3rd and 4th.

---
+++ Basics/ForCycle/ForCycle - Exercises/03. Histogram/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double p1;
+            double p2;
+            double p3;
+            double p4;
+            double p5;
+
+            int numbers;
+
+            uint counterLess200Nums = 0;
+            uint counterBetween200And399 = 0;
+            uint counterBetween400And599 = 0;
+            uint counterBetween600And799 = 0;
+            uint countet800Plus = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                numbers = int.Parse(Console.ReadLine());
+
+                if (numbers < 200)
+                {
+                    counterLess200Nums++;
+                }
+                else if (numbers >= 200 && numbers <= 399)
+                {
+                    counterBetween200And399++;
+                }
+                else if (numbers >= 400 && numbers <= 599)
+                {
+                    counterBetween400And599++;
+                }
+                else if (numbers >= 600 && numbers <= 799)
+                {
+                    counterBetween600And799++;
+                }
+                else if (numbers >= 800)
+                {
+                    countet800Plus++;
+                }
+            }
+
+            p1 = ((double)counterLess200Nums / 20 * 100);
+            p2 = ((double)counterBetween200And399 / 20 * 100 * 1.0);
+            p3 = (((double)counterBetween400And599 / 20) * 100);
+            p4 = (((double)counterBetween600And799 / 20) * 100);
+            p5 = (((double)countet800Plus / 20) * 100);
+
+            //double sumPercentages = p1 + p2 + p3 + p4+ p5;
+            //ouble percentagep1 = sumPercentages - p2 - p3 - p4 - p5;
+
+            Console.WriteLine($"{p1:F2}%");
+            Console.WriteLine($"{p2:F2}%");
+            Console.WriteLine($"{p3:F2}%");
+            Console.WriteLine($"{p4:F2}%");
+            Console.WriteLine($"{p5:F2}%");
+        }
+    }
+}
---
+++ Basics/ForCycle/ForCycle - Exercises/04. Clever Lily/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint lillyAge = uint.Parse(Console.ReadLine());
+            double washingMacninePrice = double.Parse(Console.ReadLine());
+            double singleToyPrice = double.Parse(Console.ReadLine());
+
+            int counterToys = 0;
+            int counterYearsLillyGotMoney = 0;
+            double moneyForBdays = 0;
+
+            if (lillyAge <= 77 && lillyAge >= 1)
+            {
+                for (int i = 1; i <= 77; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        counterToys++;
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        counterYearsLillyGotMoney++;
+                        moneyForBdays = moneyForBdays + 10.00;
+                    }
+                }
+            }
+
+            double allMoney = ((counterToys * singleToyPrice) + moneyForBdays) - counterYearsLillyGotMoney;
+
+            if (washingMacninePrice > allMoney)
+            {
+                double neededMoney = washingMacninePrice - allMoney;
+                Console.WriteLine($"No! {neededMoney:F2}");
+            }
+            else if (washingMacninePrice <= allMoney)
+            {
+                double neededMoney = allMoney - washingMacninePrice;
+                Console.WriteLine($"No! {neededMoney:F2}");
+            }
+        }
+    }
+}
