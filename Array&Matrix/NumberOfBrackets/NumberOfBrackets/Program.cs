aab2b369a063599cfe4b9e41bbf81aa0f4ed405e
VioletaShindova <violetashindovaa@gmail.com>
2022-06-23 23:47:17 +0300

    array and stack.

---
+++ Array&Matrix/NumberOfBrackets/NumberOfBrackets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+         Да се напише програма, която определя дали дадена последователност от скоби е правилна. Примери: 
+
+            правилно записани скоби:  (), ([]){}, ()[], (({})),
+
+            неправилно записани:  )(, (])({), (, )})), ((())].
+             */
+            Stack<char> st = new Stack<char>();
+
+            string input = Console.ReadLine();
+
+            char symbol = ' ';
+            bool isOk = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                symbol = input[i];
+                if (st.Count != 0)
+                {
+                    if (symbol == '}' || symbol == ')' || symbol == ']')
+                    {
+                        st.Pop();
+                    }
+                }
+                else if (symbol == '}' || symbol == ')' || symbol == ']')
+                {
+                    st.Pop();
+                }
+                else if (symbol == '{' || symbol == '(' || symbol == '[')
+                {
+                    st.Push(symbol);
+                }
+            }
+        }
+    }
+}
