6f6e3c6f61cc5fafe9d7ba45c53f9177464e49c2
VioletaShindova <violetashindovaa@gmail.com>
2022-12-28 17:42:26 +0200

    2nd

---
+++ Array&Matrix/SchoolЕдномерни/MultiplyingEachElement/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyingEachElement
+{
+    internal class Program
+    {
+        static void InputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return;
+        }
+
+        static void MultiplyingEachArrayElement(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] * 3;
+            }
+        }
+
+        static void OutputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input length of the array: ");
+            uint arrayLength = uint.Parse(Console.ReadLine());
+
+            int[] array = new int[arrayLength];
+
+            InputArray(array);
+
+            MultiplyingEachArrayElement(array);
+
+            Console.WriteLine("Array: ");
+            OutputArray(array);
+        }
+    }
+}
