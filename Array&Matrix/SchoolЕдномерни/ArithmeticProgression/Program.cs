e6c4d71e3d8407cea1614fa0fa22a539980a5d6d
VioletaShindova <violetashindovaa@gmail.com>
2022-12-28 22:53:08 +0200

    8th

---
+++ Array&Matrix/SchoolЕдномерни/ArithmeticProgression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticProgression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint firstElement = uint.Parse(Console.ReadLine()); //of the progression
+            int subtraction = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ Array&Matrix/SchoolЕдномерни/TwoArrays2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoArrays2
+{
+    internal class Program
+    {
+        static void InputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input length of the arrays: ");
+            uint arrayLength = uint.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[arrayLength];
+            int[] secondArray = new int[arrayLength];
+            int[] thirdArray = new int[arrayLength];
+
+            Console.WriteLine("First array: ");
+            InputArray(firstArray);
+            Console.WriteLine("Second array: ");
+            InputArray(secondArray);
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if(firstArray[i] > secondArray[i])
+                {
+                    thirdArray[i] = firstArray[i];
+                }
+                else
+                {
+                    thirdArray[i] = secondArray[i];
+                }
+            }
+
+            Console.WriteLine("Third array:");
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine(thirdArray[i] + " ");
+            }
+        }
+    }
+}
